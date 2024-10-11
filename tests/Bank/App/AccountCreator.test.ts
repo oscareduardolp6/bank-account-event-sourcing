87c2e46b5994c15ab3f065ca7a09b60aceb9ee6e
Oscar López <oscareduardolp6@gmail.com>
2024-10-11 14:42:52 -0600

    test: :test_tube: Add Creation Account Failing Tests

---
+++ tests/Bank/App/AccountCreator.test.ts
@@ -0,0 +1,15 @@
+describe('Account', () => {
+
+  it('should be created without error', () => {
+    throw new Error('Not implemented test')
+  })
+
+  it('should be fail with Account with invalid UUID', () => {
+    throw new Error('Not implemented test')
+  })
+
+  it('should not fail with an alreadoy existent account Id', () => {
+    throw new Error('Not implemented test')
+  })
+
+})
