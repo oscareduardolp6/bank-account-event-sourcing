7829ffa3941377c038e17a504189dabac27d9c5a
Oscar López <oscareduardolp6@gmail.com>
2024-10-12 17:31:40 -0600

    test: :test_tube: Added test for Get Balance use case

---
+++ src/Bank/Account/App/GetBalance/GetBalance.test.ts
@@ -0,0 +1,15 @@
+describe('Get Balance of an Account', () => {
+
+  it('should return none if the account does not exists', async () => {
+
+  })
+
+  it('should sum all the deposits', async () => {
+
+  })
+
+  it('should return 0 for just created account', async () => {
+
+  })
+
+})
